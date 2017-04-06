7177454dca295c5018c4b9ccd13ab9739930f719
Tei1988 <github@grifola.xyz>
2017-04-06 18:04:30 +0900

    apply thor, erb template, retryable and slack attachments

--- main.rb
+++ main.rb
@@ -8,89 +8,104 @@ require 'yaml'
 require 'erb'
 require 'thor'
 
+require 'pp'
+
+class OptimizelyExperiment
+  include Virtus.model
+
+  attribute :optimizely, Optimizely::Engine
+  attribute :config, Hash
+  attribute :project, Optimizely::Project
+  attribute :experiment, Optimizely::Experiment
+
+  def goal_name(goal_id)
+    @goal_names[goal_id]
+  end
+
+  def variation_name(variation_id)
+    @variation_names[variation_id]
+  end
+
+  def stats
+    @stats ||= {}.tap do |stats|
+      variation_names = {}
+      goal_names = {}
+      optimizely.stats(experiment.id).each_with_object(stats) do |s, m|
+        variation_id = s.variation_id
+        goal_id = s.goal_id
+        baseline_id = s.baseline_id
+
+        variation_names[variation_id] = s['variation_name']
+        goal_names[goal_id] = s['goal_name']
+
+        values = m.fetch(goal_id, {})
+        values[variation_id] = s
+        m[goal_id] = values
+      end
+      @goal_names = goal_names
+      @variation_names = variation_names
+    end
+  end
+end
+
+class Helper
+  def self.judge(stats)
+    stats.values
+      .select { |s| s['status'] != 'baseline' }
+      .map { |s| s['status'] }
+      .first || 'baseline'
+  end
+  def self.sort(t)
+    baseline_id = t.values.select { |s| s['status'] == 'baseline' }.first['baseline_id']
+    t.sort { |a, b| (baseline_id == a[0]) ? -1 : ((baseline_id == b[0]) ? 1 : Integer(a[0]) - Integer(b[0])) }
+  end
+  def self.url_conditions(e)
+    e['url_conditions'].map { |url| url['value'] }.join('\n')
+  end
+end
+
 class OptimizelyNotifyCLI < Thor
   option :config, aliases: :c, default: './config.yml'
-  desc '', 'fetch the results from Optimizely and notify to Slack.'
-  def execute
-    config = YAML.load(File.read(options[:config]))
+  desc 'execute', 'fetch the results from Optimizely and notify to Slack.'
 
+  def execute
     config[:target_projects].each do |target_project|
       Slack.configure { |c| c.token = config[:slack_tokens][target_project[:slack_token_ref]] }
       optimizely = Optimizely.new(api_token: config[:optimizely_tokens][target_project[:optimizely_token_ref]])
-
-      optimizely_projects = optimizely.projects
-
-      target_project_name = target_project[:project_name]
-      if target_project_name.present?
-        optimizely_projects = optimizely_projects.select { |p| target_project_name == p.project_name }
-      end
-
-      next if optimizely_projects.empty?
-
-      goal_status_icons = target_project[:goal_status_icons]
-      variation_names = {}
-      goal_names = {}
-
-      optimizely_projects.each do |project|
-        experiment_messages = optimizely
-          .experiments(project.id)
-          .select { |e| e['status'] == 'Running' }
-          .flat_map do |e|
-            experiment_id = e.id
-            baseline_id = nil
-            map = optimizely.stats(experiment_id).each_with_object({}) do |s, m|
-              variation_id = s.variation_id
-              goal_id = s.goal_id
-              baseline_id = s.baseline_id
-
-              variation_names[variation_id] = s['variation_name']
-              goal_names[goal_id] = s['goal_name']
-
-              values = m.fetch(variation_id, {})
-              values[goal_id] = s
-              m[variation_id] = values
+      Retryable.retryable(tries: 3, sleep: 5) do
+        projects(optimizely, target_project).each do |project|
+          Retryable.retryable(tries: 3, sleep: 5) do
+            optimizely.experiments(project.id).select { |e| e['status'] == 'Running' }
+            .each do |experiment|
+              Retryable.retryable(tries: 3, sleep: 5) do
+                e = OptimizelyExperiment.new(optimizely: optimizely, config: target_project, project: project, experiment: experiment)
+                object = JSON.parse(ERB.new(File.read("templates/#{target_project[:template_filepath]}"), nil, '-').result(binding), symbolize_names: true)
+                object[:attachments] = object[:attachments].to_json
+                Slack::Client.new.tap do |client|
+                  pp client.chat_postMessage(object)
+                end
+              end
             end
-
-            variation_message = map
-              .sort { |a, b| (baseline_id == a[0]) ? -1 : ((baseline_id == b[0]) ? 1 : Integer(a[0]) - Integer(b[0])) }
-              .map do |k, v|
-                goal_message = v.sort.map do |goal_id, s|
-                  <<~MESSAGE
-                  #{goal_status_icons[s['status']]}#{goal_names[goal_id]}
-                  CVR #{"%.2f" % (s['conversion_rate'] * 100)}%#{s['status'] != 'baseline' ? "(有意性確定まで#{s['visitors_until_statistically_significant']}人)" : ''}
-                  MESSAGE
-                end.join
-                <<~MESSAGE
-                ■#{variation_names[k]}
-                #{goal_message}
-                MESSAGE
-              end.join
-            <<~MESSAGE
-              ```
-              実験名: #{e['description']}
-              開始日: #{e['last_modified'].to_date} (#{(Date.current - e['last_modified'].to_date).to_i}日経過)
-              #{e['details']}
-              結果URL: #{e['shareable_results_link']}
-              対象URL:
-              #{e['url_conditions'].map { |url| '  ' + url['value'] }.join("\n")}
-              #{variation_message}
-              ```
-            MESSAGE
-        end
-
-        Slack::Client.new.tap do |client|
-          experiment_messages.each do |message|
-            client.chat_postMessage(
-              username: "Optimizely通知(#{project['project_name']})",
-              as_user: false,
-              channel: target_project[:channel],
-              text: message,
-              icon_emoji: target_project[:icon_emoji],
-            )
           end
         end
       end
     end
+  end
+
+  private
+
+  def initialize(*params)
+    super(*params)
+  end
+
+  def config
+    @config ||= YAML.load(File.read(options[:config]))
+  end
+
+  def projects(optimizely, target_project)
+    target_project_name = target_project[:project_name]
+    optimizely.projects
+      .select { |p| target_project_name.blank? || (target_project_name == p.project_name) }
   end
 end
 OptimizelyNotifyCLI.start(ARGV)
